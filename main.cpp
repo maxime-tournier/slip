83cb073fed7d1ae5647629b5b987bb5c545bb041
Maxime Tournier <maxime.tournier@anatoscope.com>
2018-07-30 14:37:11 +0200

    fixed parse bug

--- main.cpp
+++ main.cpp
@@ -7,217 +7,7 @@
 
 #include "sexpr.hpp"
 #include "environment.hpp"
-
-#include <functional>
-#include "../maybe.hpp"
-
-
-struct unimplemented : std::runtime_error {
-  unimplemented(const std::string& what)
-    : std::runtime_error("unimplemented: " + what) { }
-};
-
-namespace ast {
-
-  // expressions
-  template<class T>
-  struct lit {
-    const T value;
-  };
-
-  template<class T>
-  static lit<T> make_lit(T value) { return {value}; }
-
-  struct expr;
-
-  struct app {
-    const ref<expr> func;
-    const list<expr> args;
-  };
-
-  struct abs {
-    const list<symbol> args;
-    const list<expr> body;
-  };
-
-  struct var {
-    const symbol name;
-  };
-
-
-  // definition
-  struct def {
-    const symbol name;
-    const ref<expr> value;
-  };
-  
-  // stateful computations
-  struct io : variant<def, ref<expr> >{
-    using io::variant::variant;
-  };
-
-  // computation sequencing
-  struct seq {
-    const list<io> items;
-  };
-
-  struct expr : variant< lit<boolean>,
-                         lit<integer>,
-                         lit<real>,
-                         var, abs, app,
-                         def,
-                         seq> {
-    using expr::variant::variant;
-  };
-
-  // list destructuring monad
-  template<class U>
-  using monad = std::function<maybe<U>(const list<sexpr>& )>;
-  
-  template<class U>
-  static monad<U> pure(const U& value) {
-    return [value](const list<sexpr>&) { return value; };
-  };
-
-  // matches list head with given type and return it (maybe)
-  template<class U>
-  static maybe<U> match(const list<sexpr>& self) {
-    if(!self) return {};
-    if(auto value = self->head.template get<U>()) {
-      return *value;
-    }
-    return {};
-  }
-
-
-  template<class T>
-  struct maybe_type;
-
-  template<class T>
-  struct maybe_type<maybe<T>> {
-    using type = T;
-  };
-  
-  template<class Func>
-  struct monad_type {
-    using result_type = typename std::result_of<Func(const list<sexpr>&)>::type;
-    using type = typename maybe_type<result_type>::type;
-  };
-
-  
-  // monadic bind
-  template<class A, class F>
-  struct bind_type {
-    using source_type = typename monad_type<A>::type;
-    using result_type = typename std::result_of<F(const source_type&)>::type;
-    using value_type = typename monad_type<result_type>::type ;
-    
-    const A a;
-    const F f;
-    
-    maybe<value_type> operator()(const list<sexpr>& self) const {
-      return a(self) >> [&](const source_type& value) {
-        return f(value)(self->tail);
-      };
-    }
-  };
-
-  template<class A, class F>
-  static bind_type<A, F> bind(A a, F f) {
-    return {a, f};
-  };
-  
-
-  template<class A, class F>
-  static bind_type<A, F> operator>>(A a, F f) {
-    return {a, f};
-  };
-  
-  // coproduct
-  template<class LHS, class RHS>
-  struct coproduct_type {
-    const LHS lhs;
-    const RHS rhs;
-
-    static_assert(std::is_same<typename monad_type<LHS>::type, typename monad_type<RHS>::type>::value,
-                  "value types must agree");
-
-    using value_type = typename monad_type<LHS>::type;
-
-    maybe<value_type> operator()(const list<sexpr>& self) const {
-      if(auto value = lhs(self)) {
-        return value.get();
-      } else if(auto value = rhs(self)){
-        return value.get();
-      } else {
-        return {};
-      }
-    }
-  };
-
-  template<class LHS, class RHS>
-  static coproduct_type<LHS, RHS> operator|(LHS lhs, RHS rhs) {
-    return {lhs, rhs};
-  }
-
-  // fail
-  template<class T>
-  static maybe<T> fail(const list<sexpr>&) { return {}; }
-  
-  static expr check(const sexpr& e);
-
-  struct syntax_error : std::runtime_error {
-    using runtime_error::runtime_error;
-  };
-  
-  static maybe<expr> check_call(const list<sexpr>& args) {
-    if(!args) throw syntax_error("empty list in application");
-
-    const auto func = make_ref<expr>(check(args->head));
-    const expr res = app{func, map(args->tail, check)};
-    return res;
-  }
-
-  
-  static maybe<expr> check_abs(const list<sexpr>& self) {
-    throw unimplemented("abs");
-  }
-  
-  static const std::map<symbol, monad<expr>> special = {
-    {symbol("func"), check_abs},
-  };
-  
-  static expr check_list(list<sexpr> f) {
-    if(!f) throw syntax_error("empty list in application");
-
-    static const auto impl =
-      (match<symbol> >> [](const symbol& s) -> monad<expr> {
-        const auto it = special.find(s);
-        if(it != special.end()) return it->second;
-        return fail<expr>;
-      })
-      | check_call;
-    
-    if(auto res = impl(f)) {
-      return res.get();
-    }
-
-    throw std::runtime_error("derp");
-  };
-  
-  static expr check(const sexpr& e) {
-    return e.match<expr>
-      ([](boolean b) { return make_lit(b); },
-       [](integer i) { return make_lit(i); },
-       [](real r) { return make_lit(r); },
-       [](symbol s) { return var{s}; },
-       [](list<sexpr> f) {
-         return check_list(f);
-       });
-  }
-  
-}
-
+#include "ast.hpp"
 
 struct value;
 using env = environment<value>;
@@ -247,8 +37,6 @@ struct value : variant<real, integer, boolean, symbol, lambda, list<value> > {
 using eval_type = value (*)(const ref<env>&, const sexpr& );
 
 static std::map<symbol, eval_type> special;
-
-
 
 
 static value eval(const ref<env>& e, const sexpr& expr);
@@ -295,9 +83,22 @@ static value eval(const ref<env>& e, const sexpr& expr) {
 }
 
 
+struct history {
+  const std::string filename;
+  history(const std::string& filename="/tmp/slap.history")
+    : filename(filename) {
+    read_history(filename.c_str());
+  }
+  
+  ~history() {
+    std::clog << write_history(filename.c_str()) << std::endl;
+  }
+};
+
 template<class F>
 static void read_loop(const F& f) {
   // TODO read history
+  const history lock;
   while(const char* line = readline("> ")) {
     if(!*line) continue;
     
@@ -316,7 +117,9 @@ int main(int, char**) {
   read_loop([](std::istream& in) {
               try {
                 const sexpr s = parse(in);
-                std::cout << s << std::endl;
+                std::cout << "parsed: " << s << std::endl;
+                const ast::expr e = ast::check(s);
+                std::cout << "ast: " << e << std::endl;
               } catch(std::runtime_error& e) {
                 std::cerr << e.what() << std::endl;
               }
--- sexpr.cpp
+++ sexpr.cpp
@@ -13,14 +13,18 @@ sexpr parse(std::istream& in) {
   
   static const auto boolean_parser = true_parser | false_parser;
 
-  static const auto real_parser = parser::value<double>();  
-
-  static const auto number_parser =
+  static const auto separator_parser = // parser::debug("sep") |=
+    noskip(parser::chr<std::isspace>());
+  
+  static const auto real_parser = // parser::debug("real") |=
+    parser::value<double>();
+  
+  static const auto number_parser = // parser::debug("num") |= 
     real_parser >> [](real num) {
-                     const long cast = num;
-                     const sexpr value = num == real(cast) ? cast : num;
-                     return pure(value);                     
-                   };
+    const long cast = num;
+    const sexpr value = num == real(cast) ? cast : num;
+    return pure(value);                     
+  };
   
   static const auto initial_parser = parser::chr<std::isalpha>();
   static const auto rest_parser = parser::chr<std::isalnum>();  
@@ -28,36 +32,42 @@ sexpr parse(std::istream& in) {
   static const auto symbol_parser =
     initial_parser >>
     [](char c) {
-      return parser::noskip(*rest_parser >>
-                            [c](std::vector<char>&& rest) {
-                              const std::string tmp = c + std::string(rest.begin(),
-                                                                      rest.end());
-                              return pure(symbol(tmp));
-                            });
+      return noskip(*rest_parser >>
+                    [c](std::vector<char>&& rest) {
+                      const std::string tmp = c + std::string(rest.begin(),
+                                                              rest.end());
+                      return pure(symbol(tmp));
+                    });
     };
   
   static const auto as_expr = parser::cast<sexpr>();
 
   static auto expr_parser = parser::any<sexpr>();
 
-  static const auto separator_parser = +parser::chr<std::isspace>();
+  static const auto lparen = // parser::debug("lparen") |=
+    parser::token("(");
   
-  static const auto list_parser
-    = parser::token("(")
-    >>= ((parser::ref(expr_parser) % separator_parser)
-         | pure(std::vector<sexpr>()))
-    >> drop(parser::token(")"))
-    >> [](std::vector<sexpr>&& es) {
-         return pure(make_list(es.begin(), es.end()));
-       };
+  static const auto rparen = // parser::debug("rparen") |=
+    parser::token(")");
+
+  static const auto exprs_parser = // parser::debug("exprs") |=
+    parser::ref(expr_parser) % separator_parser;
+  
+  static const auto list_parser = // parser::debug("list") |=
+    lparen >>= exprs_parser >> drop(rparen)
+           >> [](std::vector<sexpr>&& es) {
+    return pure(make_list(es.begin(), es.end()));
+  };
   
   static const auto once =
-    (expr_parser
-     = (boolean_parser >> as_expr)
+    (expr_parser = // parser::debug("expr") |=
+     (boolean_parser >> as_expr)
      | (symbol_parser >> as_expr)
      | number_parser
      | (list_parser >> as_expr)
      , 0); (void) once;
+
+  parser::debug::stream = &std::clog;
   
   if(auto value = expr_parser(in)) {
     return value.get();
