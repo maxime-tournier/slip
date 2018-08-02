ce34ef1433e4178c5417d47740393ddeebd1b726
Maxime Tournier <tournier.maxime@gmail.com>
2018-08-02 09:45:36 +0200

    indent

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <map>
 #include <iostream>
 #include <sstream>
+#include <fstream>
 
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -17,7 +18,7 @@ struct history {
     : filename(filename) {
     read_history(filename.c_str());
   }
-  
+    
   ~history() {
     write_history(filename.c_str());
   }
@@ -25,17 +26,15 @@ struct history {
 
 template<class F>
 static void read_loop(const F& f) {
-  // TODO read history
   const history lock;
   while(const char* line = readline("> ")) {
     if(!*line) continue;
     
     add_history(line);
-	std::stringstream ss(line);
+    std::stringstream ss(line);
 	
     f(ss);
   }
-  
 };
 
 
@@ -46,30 +45,31 @@ static ref<env> prelude() {
   (*res)
     (symbol("+"), builtin([](const value* args, std::size_t count) -> value {
       if(count != 2) throw std::runtime_error("wrong arg count");
-      return *args[0].get<integer>() + *args[1].get<integer>();
-      }))
+      return args[0].cast<integer>() + args[1].cast<integer>();
+    }))
     (symbol("-"), builtin([](const value* args, std::size_t count) -> value {
       if(count != 2) throw std::runtime_error("wrong arg count");
       return args[0].cast<integer>() - args[1].cast<integer>();
-      }))
+    }))
     (symbol("="), builtin([](const value* args, std::size_t count) -> value {
       if(count != 2) throw std::runtime_error("wrong arg count");
       return args[0].cast<integer>() == args[1].cast<integer>();
-      }))
-    
+    }))
+        
     ;
   
   return res;
 };
 
 
-int main(int, char**) {
+int main(int argc, char** argv) {
 
   auto e = prelude();
   
   const bool debug = false;
-  
-  read_loop([&](std::istream& in) {
+
+  static const auto handler =
+    [&](std::istream& in) {
       try {
         const sexpr s = parse(in);
         // std::cout << "parsed: " << s << std::endl;
@@ -77,12 +77,34 @@ int main(int, char**) {
         // std::cout << "ast: " << a << std::endl;
         const value v = eval(e, a);
         std::cout << v << std::endl;
+        return true;
       } catch(ast::syntax_error& e) {
         std::cerr << "syntax error: " << e.what() << std::endl;
       } catch(std::runtime_error& e) {
         std::cerr << "runtime error: " << e.what() << std::endl;
       }
-    });
+      return false;
+    };
+
+  static const auto program =
+    [](std::istream& in) -> maybe<sexpr> {
+      
+  };
+  
+                                
+
+  
+  if(argc > 1) {
+    const char* filename = argv[1];
+    if(auto ifs = std::ifstream(filename)) {
+      return handler(ifs) ? 0 : 1;
+    } else {
+      std::cerr << "io error: " << "cannot read " << filename << std::endl;
+      return 1;
+    }
+  } else {
+    read_loop(handler);
+  }
   
   return 0;
 }
