1621c55e6bfe97340db079e6e25785f17183ddfe
Maxime Tournier <maxime.tournier@anatoscope.com>
2018-10-17 18:20:49 +0200

    *much* cleaner variants

--- variant.hpp
+++ variant.hpp
@@ -32,55 +32,7 @@ namespace {
     using other_types::construct;
   };
   
-  struct destruct {
-    using type = void;
-    template<class T>
-    void operator()(const T& self) const { self.~T(); }
-  };
 
-  struct move_construct {
-    using type = void;
-    template<class T>
-    void operator()(T& source, void* target) const {
-      new (target) T(std::move(source));
-    }
-  };
-
-  struct copy_construct {
-    using type = void;
-    
-    template<class T>
-    void operator()(T& source, void* target) const {
-      new (target) T(source);
-    }
-  };
-
-
-  struct equals {
-    using type = bool;
-    
-    template<class T, class Variant>
-    bool operator()(const T& self, const Variant& other) const {
-      return self == other.template cast<T>();
-    }
-  };
-
-
-  template<class ... Func>
-  struct overload : Func... {
-    overload(const Func& ... func) : Func(func)... { }
-  };
-
-
-  // helpers
-  struct ostream {
-    using type = void;
-    template<class T>
-    void operator()(const T& self, std::ostream& out) const {
-      out << self;
-    }
-  };
-  
 }
   
   
@@ -94,88 +46,172 @@ class variant {
 
   using helper_type = helper<0, Args...>;
 
+  template<class T, index_type index=helper_type::index((typename std::decay<T>::type*) 0)>
+  struct index_of {
+    static constexpr std::size_t value = index;
+  };
+  
 public:
 
-  // unsafe casts
-  template<class T,
-           index_type index = helper_type::index((typename std::decay<T>::type*) 0)>
+  // unsafe cast
+  template<class T, index_type index=index_of<T>::value>
   const T& cast() const {
     assert(index == this->index);
     return reinterpret_cast<const T&>(storage);
   }
 
-  template<class T,
-           index_type index = helper_type::index((typename std::decay<T>::type*) 0)>
+  template<class T, index_type index=index_of<T>::value>
   T& cast() {
     assert(index == this->index);
     return reinterpret_cast<T&>(storage);
   }
 
-  
   // accessors
-  template<class T,
-           index_type index = helper_type::index((typename std::decay<T>::type*) 0)>
+  template<class T, index_type index=index_of<T>::value>
   const T* get() const {
     if(this->index == index) { return &cast<T>(); }
     return nullptr;
   }
 
-  template<class T,
-           index_type index = helper_type::index((typename std::decay<T>::type*)0)>
+  template<class T, index_type index=index_of<T>()>
   T* get() {
     if(this->index == index) { return &cast<T>(); }
     return nullptr;
   }
 
   // constructors/destructors
-  template<class T,
-           index_type index = helper_type::index((typename std::decay<T>::type*) 0)>
+  template<class T, index_type index=index_of<T>::value>
   variant(T&& value) : index(index) {
     helper_type::construct(&storage, std::forward<T>(value));
   }
 
-  // TODO check whether this one actually works (visit is const)
   variant(variant&& other) : index(other.index) {
-    other.visit(move_construct(), &storage);
+    match([&](const Args& self) {
+        new (&storage) Args(std::move(other.cast<Args>()));
+      }...);
   }
   
   variant(const variant& other) : index(other.index) {
-    other.visit(copy_construct(), &storage);
+    match([&](const Args& self) {
+        new (&storage) Args(other.cast<Args>());
+      }...);
   }
 
-  ~variant() { visit(destruct()); }
+  ~variant() {
+    match([](const Args& self) { self.~Args(); }...);
+  }
 
-  // visitors
+  // visitors (const)
   template<class Func, class ... Params>
   auto visit(Func&& func, Params&& ... params) const {
     using ret_type = typename std::common_type<decltype(func(this->template cast<Args>(),
                                                              std::forward<Params>(params)...))...>::type;
-    using self_type = const variant&;
+    using self_type = decltype(*this);
     using thunk_type = ret_type (*) (self_type&&, Func&&, Params&&...);
     
     static const thunk_type table[] = {
-      [](self_type self, Func&& func, Params&& ... params) -> ret_type {
-        return std::forward<Func>(func)(self.cast<Args>(), std::forward<Params>(params)...);
+      [](self_type& self, Func&& func, Params&& ... params) -> ret_type {
+        return std::forward<Func>(func)(self.template cast<Args>(), std::forward<Params>(params)...);
+      }...
+    };
+     
+    return table[index](*this, std::forward<Func>(func), std::forward<Params>(params)...);
+  }
+
+  // visitors (non-const)
+  template<class Func, class ... Params>
+  auto visit(Func&& func, Params&& ... params) {
+    using ret_type = typename std::common_type<decltype(func(this->template cast<Args>(),
+                                                             std::forward<Params>(params)...))...>::type;
+    using self_type = decltype(*this);
+    using thunk_type = ret_type (*) (self_type&&, Func&&, Params&&...);
+    
+    static const thunk_type table[] = {
+      [](self_type& self, Func&& func, Params&& ... params) -> ret_type {
+        return std::forward<Func>(func)(self.template cast<Args>(), std::forward<Params>(params)...);
       }...
     };
     
     return table[index](*this, std::forward<Func>(func), std::forward<Params>(params)...);
   }
 
+  // pattern matching with lambdas
   template<class ... Func>
   auto match(const Func& ... func) const {
-    return visit(overload<Func...>(func...));
+    struct overload : Func... {
+      overload(const Func& ... func) : Func(func)... { }
+    };
+    
+    return visit(overload(func...));
   }
 
+  template<class ... Func>
+  auto match(const Func& ... func) {
+    struct overload : Func... {
+      overload(const Func& ... func) : Func(func)... { }
+    };
+
+    return visit(overload(func...));
+  }
+
+  // copy assignment
+  variant& operator=(const variant& other) {
+    if(this == &other) return *this;
+    
+    if(index != other.index) {
+      // destuct, change type, copy construct      
+      this->~variant();
+      index = other.index;
+      match([&](const Args& self) {
+          new (&storage) Args(other.cast<Args>());
+        }...);
+    } else {
+      // copy assign
+      match([&](Args& self) {
+          self = other.cast<Args>();
+        }...);
+    }
+
+    return *this;
+  }
+
+  // move-assignment
+  variant& operator=(variant&& other) {
+    if(this == &other) return *this;
+    
+    if(index != other.index) {
+      // destuct, change type, copy construct      
+      this->~variant();
+      index = other.index;
+      match([&](const Args& self) {
+          new (&storage) Args(std::move(other.cast<Args>()));
+        }...);
+    } else {
+      // copy assign
+      match([&](Args& self) {
+          self = std::move(other.cast<Args>());
+        }...);
+    }
+
+    return *this;
+  }
+  
+
+
+  // convenience
   friend std::ostream& operator<<(std::ostream& out, const variant& self) {
-    self.visit(ostream(), out);
+    self.match([&](const Args& self) {
+        out << self;
+      }...);
     return out;
   }
 
 
   bool operator==(const variant& other) const {
     if(other.index != index) return false;
-    return visit(equals(), other);
+    return match([&](const Args& self) {
+        return self == other.cast<Args>();
+      }...);
   }
 
   bool operator!=(const variant& other) const {
@@ -183,16 +219,6 @@ public:
   }
 
 
-  // assignment
-  variant& operator=(const variant& other) {
-    if(this != &other) {
-      // TODO optimize when types match
-      this->~variant();
-      index = other.index;
-      other.visit(copy_construct(), &storage);
-    }
-    return *this;
-  }
 
 };
 
