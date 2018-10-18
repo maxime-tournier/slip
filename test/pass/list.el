d81ce274228945bcdc196553dc121ea94dd6c5ed
Maxime Tournier <maxime.tournier@anatoscope.com>
2018-10-18 17:13:35 +0200

    comments

--- test/pass/list.el
+++ test/pass/list.el
@@ -1,19 +1,26 @@
 
 
+;; list concatenation
 (def (concat lhs rhs)
      (match lhs
             (cons self (cons self.head (concat self.tail rhs)))
             (nil _ rhs)))
 
+;; build a list containing an integer range
 (def (range start end)
      (if (= start end) nil
        (cons start (range (+ start 1) end))))
 
-(def test (concat (range 0 10) (range 0 3)))
 
+;; test list
+(def test (concat (range 0 10) (range 0 3)))
+test
+
+;; list map
 (def (map f x)
      (match x
             (nil _ nil)
             (cons self (cons (f self.head) (map f self.tail)))))
 
+;; doubling our test list
 (map (fn (x) (* 2 x)) test)
