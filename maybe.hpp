8ae683226c7535e68b8cfd59b36482076ffe0711
Maxime Tournier <maxime.tournier@anatoscope.com>
2018-10-18 17:34:49 +0200

    maybe assignments

--- maybe.hpp
+++ maybe.hpp
@@ -25,58 +25,93 @@ struct none { };
 
 template<class T>
 class maybe {
-    typename std::aligned_union<0, T>::type storage;
-    const bool set;
+  typename std::aligned_union<0, T>::type storage;
+  bool set;
 public:
-    using value_type = T;
-    
-    // monadic return (none/just)
-    maybe(none = {}) : set(false) { }
-    maybe(const T& value) : set(true) { new (&storage) T(value); }
-    maybe(T&& value) : set(true) { new (&storage) T(std::move(value)); }
-
-    maybe(maybe&& other) : set(other.set) {
-      if(set) new (&storage) T(std::move(other.get()));
-    }
-
-    maybe(const maybe& other) : set(other.set) {
-      if(set) new (&storage) T(other.get());
-    }
+  using value_type = T;
   
-    // unclear whether we actually need these at all
-    maybe& operator=(maybe&& other) = delete; 
-    maybe& operator=(const maybe& other) = delete;
-    
-    // monadic bind
-    template<class F>
-    using bind_type = typename std::result_of<F (const T& )>::type;
-    
-    template<class F>
-    typename detail::require_maybe< bind_type<F> >::type
-    operator>>(const F& f) const {
-        if(!set) return {};
-        return f(get());
+  // monadic return (none/just)
+  maybe(none = {}) : set(false) { }
+  maybe(const T& value) : set(true) { new (&storage) T(value); }
+  maybe(T&& value) : set(true) { new (&storage) T(std::move(value)); }
+
+  maybe(maybe&& other) : set(other.set) {
+    if(set) new (&storage) T(std::move(other.get()));
+
+    // note: moved from gets unset
+    other.set = false;
+  }
+
+  maybe(const maybe& other) : set(other.set) {
+    if(set) new (&storage) T(other.get());
+  }
+  
+  // assignment
+  maybe& operator=(maybe&& other) {
+    if(set == other.set) {
+      if(set) {
+        get() = std::move(other.get());
+      }
+    } else {
+      if(set) {
+        get().~T();
+      } else {
+        new (&storage) T(std::move(other.get()));
+      }
+      set = other.get;
     }
 
-    // TODO bind from temporary
+    // note: moved from gets unset
+    other.set = false;
+    return *this;
+  }
+
+  maybe& operator=(const maybe& other) {
+    if(set == other.set) {
+      if(set) {
+        get() = other.get();
+      }
+    } else {
+      if(set) {
+        get().~T();
+      } else {
+        new (&storage) T(other.get());
+      }
+      set = other.get;
+    }
     
-    ~maybe() { 
-        if(set) get().~T();
-    }
-
-
-    // monad escape
-    explicit operator bool () const { return set; }
+    return *this;
+  }
     
-    const T& get() const {
-        if(!set) throw std::runtime_error("accessing none");
-        return *reinterpret_cast<const T*>(&storage);
-    }
+  // monadic bind
+  template<class F>
+  using bind_type = typename std::result_of<F (const T& )>::type;
+    
+  template<class F>
+  typename detail::require_maybe< bind_type<F> >::type
+  operator>>(const F& f) const {
+    if(!set) return {};
+    return f(get());
+  }
 
-    T& get() {
-        if(!set) throw std::runtime_error("accessing none");
-        return *reinterpret_cast<T*>(&storage);
-    }
+  // TODO bind from temporary
+    
+  ~maybe() { 
+    if(set) get().~T();
+  }
+  
+  // monad escape
+  explicit operator bool () const { return set; }
+    
+  const T& get() const {
+    if(!set) throw std::runtime_error("accessing none");
+    return *reinterpret_cast<const T*>(&storage);
+  }
+
+  T& get() {
+    if(!set) throw std::runtime_error("accessing none");
+    return *reinterpret_cast<T*>(&storage);
+  }
 
 };
 
