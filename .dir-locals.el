dfaecdc3766fc785dcf6ce67a174f7dd639c3bc3
Maxime Tournier <maxime.tournier@anatoscope.com>
2018-10-19 16:11:14 +0200

    .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+((nil . ((fill-column . 80)))
+ 
+ (python-mode . ((indent-tabs-mode . nil)
+                 (python-indent-offset . 4)))
+ 
+ (c++-mode . ((c-basic-offset . 2)
+              (indent-tabs-mode . nil)
+              (c-offset-alist . ((innamespace . +)
+                                 (inlambda . 0)))
+              )
+           )
+ )
