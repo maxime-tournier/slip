99619b6b18c1b6fe152fe5dacc5ec0ede37e6fc5
Maxime Tournier <tournier.maxime@gmail.com>
2018-11-01 12:42:29 +0100

    stack allocator

--- stack.hpp
+++ stack.hpp
@@ -5,7 +5,7 @@
 #include <cassert>
 #include <type_traits>
 
-template<class T>
+template<class T, std::size_t align=alignof(T)>
 class stack {
   using value_type = typename std::aligned_union<0, T>::type;
 
@@ -14,6 +14,9 @@ class stack {
 
   std::size_t sp;
 public:
+  stack(const stack& other) = default;
+  stack(stack&& other) = default;
+  
   T* top() { return reinterpret_cast<T*>(&storage[sp]); }
   
   stack(std::size_t size) : storage(size), sp(0) { }
@@ -22,7 +25,7 @@ public:
     T* res = top();
     sp += n;
     assert(sp <= storage.size() && "stack overflow");
-    
+
     return res;
   }
 
@@ -31,18 +34,33 @@ public:
     sp -= n;
     assert(ptr == top() && "stack corruption");
   }
-  
-  struct allocator {
-    stack& storage;
 
-    using value_type = T;
-    template<class U> struct rebind { using other = typename stack<U>::allocator; };
+  template<class U=T>
+  struct allocator {
+    static_assert(sizeof(U) >= sizeof(T), "size error");
+    using storage_type = stack;
+    storage_type& storage;
+
+    using value_type = U;
     
-    T* allocate(std::size_t n) { return storage.allocate(n); }
-    void deallocate(T* ptr, std::size_t n) { return storage.deallocate(ptr, n); }    
+    template<class V> struct rebind { using other = typename stack<V>::allocator; };
+    
+    U* allocate(std::size_t n) {
+      std::size_t m = (sizeof(U) * n) / sizeof(T);
+      m += (m * sizeof(T) < n * sizeof(U));
+      assert(m * sizeof(T) >= n * sizeof(U));
+      return reinterpret_cast<U*>(storage.allocate(m));
+    }
+    
+    void deallocate(U* ptr, std::size_t n) {
+      return storage.deallocate(ptr, n);
+    }    
   };
   
 };
+
+template<class T, class U=T>
+using stack_allocator = typename stack<T>::template allocator<U>;
 
 
 #endif
